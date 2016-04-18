030dd8aa6448a82ece3fd620c4488a59237e9d28
oliver1094 <futpiz_5678@hotmail.com>
2016-04-17 19:46:38 -0500

    first commit

---
+++ web/js/notification.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+    var socket = io.connect('http://localhost:8890');
+
+    socket.on('notification', function (data) {
+
+        var message = JSON.parse(data);
+
+        $( "#notifications" ).prepend( "<p><strong>" + message.name + "</strong>: " + message.message + "</p>" );
+
+    });
+
+});
